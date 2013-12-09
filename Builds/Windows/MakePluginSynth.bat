50eb657496b5b81df1b496b41543404bd62d58b5
cabbageaudio <rorywalsh@gmail.com>
2013-12-09 21:42:14 +0000

    updating bat files for windows..

--- Builds/Windows/MakePluginSynth.bat
+++ Builds/Windows/MakePluginSynth.bat
@@ -7,7 +7,7 @@ ECHO * to the Csound library                           *
 ECHO ===================================================
 ECHO 
 set JuceLibraryCode=C:\Users\Rory\Documents\SourceCode\cabbageaudio\cabbage\JuceLibraryCode
-set PreProcessorDefs=-D__MINGW__=1 -D__MINGW_EXTENSION= -DCSOUND6 -D Cabbage_Plugin_Synth=1 -DUSE_DOUBLE=1 -DCabbage_GUI_Editor=1 -DJUCER_CODEBLOCKS_20734A5D=1
+set PreProcessorDefs=-D__MINGW__=1 -DJUCE_MINGW -D__MINGW_EXTENSION= -DCSOUND6 -D Cabbage_Plugin_Synth=1 -DUSE_DOUBLE=1 -DJUCER_CODEBLOCKS_20734A5D=1
 set CsoundIncludeDir=C:\Users\Rory\Documents\SourceCode\cabbageaudio\csound6-git\include
 set ASIOSDK2Dir=C:\SDKs\ASIOSDK2.3\common
 set VSTSDKDir=C:\SDKs\vstsdk2.4
@@ -16,9 +16,8 @@ set CsoundLibrary=C:\Users\Rory\Documents\SourceCode\cabbageaudio\csound6-git\bu
 set OUTPUT_TARGET=CabbagePluginSynth.dll
 ECHO on
 
-g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %JuceLibraryCode%\modules\juce_audio_plugin_client\utility\juce_PluginUtilities.cpp -o build\intermediate\Debug\juce_PluginUtilities.o
-
-g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %JuceLibraryCode%\modules\juce_audio_plugin_client\VST\juce_VST_Wrapper.cpp -o build\intermediate\Debug\juce_VST_Wrapper.o
+rm -rf build\intermediate\Debug
+mkdir build\intermediate\Debug
 
 g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs% -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %JuceLibraryCode%\modules\juce_audio_basics\juce_audio_basics.cpp -o build\intermediate\Debug\juce_audio_basics.o
 
@@ -46,23 +45,27 @@ g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 
 
 g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %CabbageSourceDir%\BinaryData.cpp -o build\intermediate\Debug\BinaryData.o
 
-g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %CabbageSourceDir%\CabbageCallOutBox.cpp -o build\intermediate\Debug\CabbageCallOutBox.o
-
 g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %CabbageSourceDir%\CabbageGUIClass.cpp -o build\intermediate\Debug\CabbageGUIClass.o
 
 g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %CabbageSourceDir%\CabbageLookAndFeel.cpp -o build\intermediate\Debug\CabbageLookAndFeel.o
 
 g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %CabbageSourceDir%\CabbageMainPanel.cpp -o build\intermediate\Debug\CabbageMainPanel.o
 
-g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %CabbageSourceDir%\CabbagePropertiesDialog.cpp -o build\intermediate\Debug\CabbagePropertiesDialog.o
-
 g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %CabbageSourceDir%\CabbageTable.cpp -o build\intermediate\Debug\CabbageTable.o
 
 g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %CabbageSourceDir%\csPerfThread.cpp -o build\intermediate\Debug\csPerfThread.o
 
+g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c C:\Users\Rory\Documents\SourceCode\cabbageaudio\cabbage\Source\CabbageCallOutBox.cpp -o build\intermediate\Debug\CabbageCallOutBox.o
+
+g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c C:\Users\Rory\Documents\SourceCode\cabbageaudio\cabbage\Source\ComponentLayoutEditor.cpp -o build\intermediate\Debug\ComponentLayoutEditor.o
+
+g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c C:\Users\Rory\Documents\SourceCode\cabbageaudio\cabbage\Source\CabbagePropertiesDialog.cpp -o build\intermediate\Debug\CabbagePropertiesDialog.o
+
 g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %CabbageSourceDir%\DirectoryContentsComponent.cpp -o build\intermediate\Debug\DirectoryContentsComponent.o
 
 g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %CabbageSourceDir%\Plugin\CabbagePluginProcessor.cpp -o build\intermediate\Debug\CabbagePluginProcessor.o
+
+g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %CabbageSourceDir%\Plugin\CabbagePluginEditor.cpp -o build\intermediate\Debug\CabbagePluginEditor.o
 
 g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %CabbageSourceDir%\Soundfiler.cpp -o build\intermediate\Debug\Soundfiler.o
 
@@ -70,4 +73,8 @@ g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 
 
 g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %CabbageSourceDir%\XYPadAutomation.cpp -o build\intermediate\Debug\XYPadAutomation.o
 
-g++.exe -shared -Wl,--dll -o build\%OUTPUT_TARGET% build\intermediate\Debug\juce_audio_basics.o build\intermediate\Debug\juce_audio_devices.o build\intermediate\Debug\juce_audio_formats.o build\intermediate\Debug\juce_audio_processors.o build\intermediate\Debug\juce_audio_utils.o build\intermediate\Debug\juce_core.o build\intermediate\Debug\juce_cryptography.o build\intermediate\Debug\juce_data_structures.o build\intermediate\Debug\juce_events.o build\intermediate\Debug\juce_graphics.o build\intermediate\Debug\juce_gui_basics.o build\intermediate\Debug\juce_gui_extra.o build\intermediate\Debug\juce_PluginUtilities.o build\intermediate\Debug\juce_VST_Wrapper.o build\intermediate\Debug\BinaryData.o build\intermediate\Debug\CabbageCallOutBox.o build\intermediate\Debug\CabbageGUIClass.o build\intermediate\Debug\CabbageLookAndFeel.o build\intermediate\Debug\CabbageMainPanel.o build\intermediate\Debug\CabbagePropertiesDialog.o build\intermediate\Debug\CabbageTable.o build\intermediate\Debug\csPerfThread.o build\intermediate\Debug\DirectoryContentsComponent.o build\intermediate\Debug\CodeWindow.o build\intermediate\Debug\CommandManager.o build\intermediate\Debug\CabbagePluginProcessor.o build\intermediate\Debug\Soundfiler.o build\intermediate\Debug\XYPad.o build\intermediate\Debug\XYPadAutomation.o -lgdi32 -luser32 -lkernel32 -lcomctl32 -lcomdlg32 -limm32 -lole32 -loleaut32 -lrpcrt4 -lshlwapi -luuid -lversion -lwininet -lwinmm -lws2_32 -lwsock32 %CsoundLibrary%  -mwindows cabbage.res
+g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %JuceLibraryCode%\modules\juce_audio_plugin_client\utility\juce_PluginUtilities.cpp -o build\intermediate\Debug\juce_PluginUtilities.o
+
+g++.exe -Wall -Wno-strict-aliasing -Wno-strict-overflow  -march=pentium4 -g -O0 -std=gnu++0x -mstackrealign %PreProcessorDefs%    -I. -I..\..\JuceLibraryCode -I%CsoundIncludeDir% -I%ASIOSDK2Dir% -I%VSTSDKDir%  -c %JuceLibraryCode%\modules\juce_audio_plugin_client\VST\juce_VST_Wrapper.cpp -o build\intermediate\Debug\juce_VST_Wrapper.o
+
+g++.exe -shared -Wl,--dll -o build\%OUTPUT_TARGET% build\intermediate\Debug\juce_audio_basics.o build\intermediate\Debug\juce_audio_devices.o build\intermediate\Debug\juce_audio_formats.o build\intermediate\Debug\juce_audio_processors.o build\intermediate\Debug\juce_audio_utils.o build\intermediate\Debug\juce_core.o build\intermediate\Debug\juce_cryptography.o build\intermediate\Debug\juce_data_structures.o build\intermediate\Debug\juce_events.o build\intermediate\Debug\juce_graphics.o build\intermediate\Debug\juce_gui_basics.o build\intermediate\Debug\juce_gui_extra.o build\intermediate\Debug\juce_PluginUtilities.o build\intermediate\Debug\juce_VST_Wrapper.o build\intermediate\Debug\BinaryData.o build\intermediate\Debug\CabbageCallOutBox.o build\intermediate\Debug\CabbagePluginEditor.o build\intermediate\Debug\CabbageGUIClass.o build\intermediate\Debug\CabbageLookAndFeel.o build\intermediate\Debug\CabbageMainPanel.o build\intermediate\Debug\CabbageTable.o build\intermediate\Debug\csPerfThread.o build\intermediate\Debug\DirectoryContentsComponent.o build\intermediate\Debug\CabbagePropertiesDialog.o build\intermediate\Debug\ComponentLayoutEditor.o build\intermediate\Debug\CabbagePluginProcessor.o build\intermediate\Debug\Soundfiler.o build\intermediate\Debug\XYPad.o build\intermediate\Debug\XYPadAutomation.o -lgdi32 -luser32 -lkernel32 -lcomctl32 -lcomdlg32 -limm32 -lole32 -loleaut32 -lrpcrt4 -lshlwapi -luuid -lversion -lwininet -lwinmm -lws2_32 -lwsock32 %CsoundLibrary%  -mwindows cabbage.res

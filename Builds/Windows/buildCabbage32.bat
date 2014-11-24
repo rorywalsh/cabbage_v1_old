40361d91f6a864a65947abf4f177efec3d844f18
cabbageaudio <rorywalsh@ear.ie>
2014-11-24 15:35:42 +0000

    tidying up windows make files

--- Builds/Windows/buildCabbage32.bat
+++ Builds/Windows/buildCabbage32.bat
@@ -1,10 +1,10 @@
 ECHO "Build script for Cabbage on Windows. See README.txt for more information"
 
-CALL mingw32-make -f MakefileStandalone64.mak CONFIG=Release clean
-CALL mingw32-make -f MakefileStandalone64.mak CONFIG=Release
-CALL mingw32-make -f MakefilePluginEffect64.mak CONFIG=Release clean
-CALL mingw32-make -f MakefilePluginEffect64.mak CONFIG=Release
-CALL mingw32-make -f MakefilePluginSynth64.mak CONFIG=Release clean
-CALL mingw32-make -f MakefilePluginSynth64.mak CONFIG=Release
+CALL mingw32-make -f MakefileStandalone.mak CONFIG=Release clean
+CALL mingw32-make -f MakefileStandalone.mak CONFIG=Release -j4
+CALL mingw32-make -f MakefilePluginEffect.mak CONFIG=Release clean
+CALL mingw32-make -f MakefilePluginEffect.mak CONFIG=Release -j4
+CALL mingw32-make -f MakefilePluginSynth.mak CONFIG=Release clean
+CALL mingw32-make -f MakefilePluginSynth.mak CONFIG=Release -j4
 CALL mv build\CabbagePluginEffect.dll build\CabbagePluginEffect.dat 
 CALL mv build\CabbagePluginSynth.dll build\CabbagePluginSynth.dat
--- Builds/Windows/buildCabbage64.bat
+++ Builds/Windows/buildCabbage64.bat
@@ -1,10 +1,10 @@
 ECHO "Build script for Cabbage on Windows. See README.txt for more information"
 
 CALL mingw32-make -f MakefileStandalone64.mak CONFIG=Release clean
-CALL mingw32-make -f MakefileStandalone64.mak CONFIG=Release
+CALL mingw32-make -f MakefileStandalone64.mak -j4 CONFIG=Release
 CALL mingw32-make -f MakefilePluginEffect64.mak CONFIG=Release clean
-CALL mingw32-make -f MakefilePluginEffect64.mak CONFIG=Release
+CALL mingw32-make -f MakefilePluginEffect64.mak -j4 CONFIG=Release
 CALL mingw32-make -f MakefilePluginSynth64.mak CONFIG=Release clean
-CALL mingw32-make -f MakefilePluginSynth64.mak CONFIG=Release
+CALL mingw32-make -f MakefilePluginSynth64.mak -j4 CONFIG=Release
 CALL mv build\CabbagePluginEffect.dll build\CabbagePluginEffect.dat 
 CALL mv build\CabbagePluginSynth.dll build\CabbagePluginSynth.dat

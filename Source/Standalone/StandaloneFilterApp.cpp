9ba43d389655d5275a86bb39e9b815ba22b41cce
rorywalsh <rorywalsh@ear.ie>
2015-09-23 19:28:24 +0100

    fixing some OSX issues

--- Source/Standalone/StandaloneFilterApp.cpp
+++ Source/Standalone/StandaloneFilterApp.cpp
@@ -44,7 +44,7 @@ public:
 #if defined(LINUX) || defined(MACOSX)
         manualPath = File::getSpecialLocation(File::currentExecutableFile).getParentDirectory().getFullPathName()+"/CsoundDocs/index.html";
 #else
-        manualPath = "Library/Frameworks/CsoundLib64.framework/Versions/6.0/Resources/Manual/index.html";
+        manualPath = "Library/Frameworks/CsoundLib64.framework/Versions/6.0/Resources/Manual/";
 #endif
         defaultPropSet->setValue("CsoundHelpDir", manualPath);
         defaultPropSet->setValue("PlantFileDir", homeDir+"/Plants");
@@ -53,13 +53,13 @@ public:
         defaultPropSet->setValue("ShowEditorConsole", 1);
         defaultPropSet->setValue("ExternalEditor", 0);
         defaultPropSet->setValue("UseCabbageIO", 1);
-        defaultPropSet->setValue("ShowConsoleWithEditor", 0);
+        defaultPropSet->setValue("ShowConsoleWithEditor", 1);
         defaultPropSet->setValue("UsingCabbageCsound", 1);
         defaultPropSet->setValue("AudioEnabled", 1);
         defaultPropSet->setValue("DisableGUIEditModeWarning", 1);
         defaultPropSet->setValue("SetAlwaysOnTop", 1);
         defaultPropSet->setValue("PlantRepository", xml);
-        defaultPropSet->setValue("EditorColourScheme", 1);
+        defaultPropSet->setValue("EditorColourScheme", 0);
         defaultPropSet->setValue("showTabs", 1);
         defaultPropSet->setValue("EnablePopupDisplay", 1);
         defaultPropSet->setValue("ShowNativeFileDialogues", 1);

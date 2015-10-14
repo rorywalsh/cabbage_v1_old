12e178687defd789f8d9da902cbda8f1a52864e3
rorywalsh <rorywalsh@ear.ie>
2015-10-14 13:07:21 +0100

    updating osx build

---
+++ Builds/MacOSX/Test/Test.h
@@ -0,0 +1,19 @@
+//
+//  Test.h
+//  Test
+//
+//  Created by Walshr on 13/10/2015.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Test.
+FOUNDATION_EXPORT double TestVersionNumber;
+
+//! Project version string for Test.
+FOUNDATION_EXPORT const unsigned char TestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Test/PublicHeader.h>
+
+

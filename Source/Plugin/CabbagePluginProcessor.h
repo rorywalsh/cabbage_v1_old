ac0989f72b40ee113ac95a0694333bbe07b12fa6
rorywalsh <rorywalsh@ear.ie>
2015-11-10 09:47:02 +0000

    updating processor

--- Source/Plugin/CabbagePluginProcessor.h
+++ Source/Plugin/CabbagePluginProcessor.h
@@ -461,7 +461,11 @@ public:
 #ifdef WIN32
         String opcodeDir = File::getSpecialLocation(File::currentExecutableFile).getParentDirectory().getFullPathName()+"\\CsoundPlugins";
         if(!File(opcodeDir).exists())
+        #ifdef Cabbage64Bit
+            opcodeDir = String(getenv("CABBAGE_OPCODE_PATH64"));
+        #else
             opcodeDir = String(getenv("CABBAGE_OPCODE_PATH"));
+        #endif
 
         Logger::writeToLog("\n================================\nCabbage opcode plugins are located at:"+opcodeDir);
         //showMessage(opcodeDir);

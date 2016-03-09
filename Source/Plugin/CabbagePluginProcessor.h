303d8d00414ab005deea245c2af7398f30feae50
rorywalsh <rorywalsh@ear.ie>
2016-03-09 11:51:26 +0000

    updated minor build version number

--- Source/Plugin/CabbagePluginProcessor.h
+++ Source/Plugin/CabbagePluginProcessor.h
@@ -51,9 +51,9 @@ class CodeWindow;
 #endif
 
 #ifdef Cabbage64Bit
-#define CABBAGE_VERSION "Cabbage(64bit) v1.0.02"
+#define CABBAGE_VERSION "Cabbage(64bit) v1.0.03"
 #else
-#define CABBAGE_VERSION "Cabbage(32bit) v1.0.02"
+#define CABBAGE_VERSION "Cabbage(32bit) v1.0.03"
 #endif
 
 #define AUDIO_PLUGIN 1

a66c30e5d6ac07b7647cc522b970af9fa5b4002d
etwete <rorywalsh@ear.ie>
2016-11-18 14:15:41 +0000

    commiting

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -236,13 +236,13 @@
 	#endif
 #else
 	#ifndef  JucePlugin_MaxNumInputChannels
-	#define JucePlugin_MaxNumInputChannels    8
+	#define JucePlugin_MaxNumInputChannels    2
 	#endif
 	#ifndef  JucePlugin_MaxNumOutputChannels
-	#define JucePlugin_MaxNumOutputChannels   8
+	#define JucePlugin_MaxNumOutputChannels   2
 	#endif
 	#ifndef  JucePlugin_PreferredChannelConfigurations
-	#define JucePlugin_PreferredChannelConfigurations  {2,2}, {4,4}
+	#define JucePlugin_PreferredChannelConfigurations  {2,2}
 	#endif
 #endif
 
